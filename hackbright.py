196ecaecda2728ecbe372a706b7046456a77e7ae
vagrant <vagrant@vagrant.vm>
2018-09-29 05:23:33 +0000

    add to zero

--- hackbright.py
+++ hackbright.py
@@ -1,5 +1,45 @@
 def has_balanced_parens(phrase):
-    """Does a string have balanced parentheses?"""
+    """Does a string have balanced parentheses?
+    These are fine::
+
+       >>> has_balanced_brackets("<ok>")
+       True
+
+       >>> has_balanced_brackets("<{ok}>")
+       True
+
+       >>> has_balanced_brackets("<[{(yay)}]>")
+       True
+
+    These are invalid, since they have too many open brackets::
+
+       >>> has_balanced_brackets("(Oops!){")
+       False
+
+       >>> has_balanced_brackets("{[[This has too many open square brackets.]}")
+       False
+
+    These are invalid, as they close brackets that weren't open::
+
+       >>> has_balanced_brackets(">")
+       False
+
+       >>> has_balanced_brackets("(This has {too many} ) closers. )")
+       False
+
+    Here's a case where the number of brackets opened matches
+    the number closed, but in the wrong order::
+
+        >>> has_balanced_brackets("<{Not Ok>}")
+        False
+
+    If you receive a string with no brackets, consider it balanced::
+
+       >>> has_balanced_brackets("No brackets here!")
+       True
+
+
+    """
     # stack = []
 
     # for k in phrase:
@@ -41,7 +81,7 @@ def has_balanced_brackets(phrase):
        >>> has_balanced_brackets("<[{(yay)}]>")
        True
 
-    These are invalid, since they have too many open brackets::
+    These are invalid, since they have too many open brackets:
 
        >>> has_balanced_brackets("(Oops!){")
        False
@@ -71,6 +111,7 @@ def has_balanced_brackets(phrase):
     """
     brackets = {')':'(','}':'{',']':'[','>':'<'}
     brackets_set = set(brackets.values())
+
     stack = []
     for l in phrase:
         if l in brackets_set:
@@ -79,8 +120,36 @@ def has_balanced_brackets(phrase):
             if stack == [] or brackets[l] != stack.pop():
                 return False
     return stack == []
-    
+
+def add_to_zero(nums):
+    """Given list of ints, return True if any two nums sum to 0.
+    >>> add_to_zero([])
+    False
+
+    >>> add_to_zero([1])
+    False
+
+    >>> add_to_zero([1, 2, 3])
+    False
+
+    >>> add_to_zero([1, 2, 3, -2])
+    True
+
+    >>> add_to_zero([0, 1, 2])
+    True
+
+    """
+    for i in nums:
+        for j in nums:
+            if i + j == 0:
+                return True
+    return False
+
+
+
+
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
-        print("\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY BRACKETS!\n")
+        print("\n*** ALL TESTS PASSED.\n")
